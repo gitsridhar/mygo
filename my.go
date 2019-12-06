f7aba671411f0235b0b66ee819c6d9c01b5e04be
gitsridhar <sridapple@gmail.com>
2019-12-06 10:46:57 -0500

    Update my.go

--- my.go
+++ my.go
@@ -1,10 +1,11 @@
 package main
 
 import "fmt"
+import "strconv"
 
 func main() {
     fmt.Println("hello world")
     for i:= 0; ; i++ {
-        fmt.Println("Doing it again " + i)
+        fmt.Println("Doing it again " + strconv.Itoa(i))
     }
 }
